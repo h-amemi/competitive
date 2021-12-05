08896a85b4eca3a1329d453e8a10373fc995af79
Hayato Amemiya <hayato.ame@gmail.com>
2021-12-05 18:00:52 +0900

    Add haskell/2019-yo/1.js

---
+++ haskell/2019-yo/1.hs
@@ -0,0 +1,20 @@
+toInt :: (String -> Int)
+toInt x = read x :: Int
+
+getInts = do
+  x <- getLine
+  let y = (map $toInt) $words x
+  return y
+
+main = do
+  x <- getInts
+
+  let a = head x
+  let b = x !! 1
+  let c = last x
+
+  let coinPerWeek = 7 * a + b
+  let week = c `div` coinPerWeek
+  let day = ceiling (fromIntegral (c - coinPerWeek * week) / fromIntegral a)
+
+  print $ 7 * week + day
